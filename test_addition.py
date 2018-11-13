f006a4c1d5b6d4cffed2efae63f16bb408d06cb5
nkhadka <nbnnbnkhadka@gmail.com>
2018-11-13 10:43:28 +0545

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from somepackage.helper import give_reverse
+
 def test_add():
     assert 5 == (3+2)
+    
+def test_give_reverse():
+    assert give_reverse('nabin') == 'niban'
